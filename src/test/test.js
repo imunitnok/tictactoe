e4b992558606b68ed05c9755d57835f1341b42cd
Timofei Shugailo <shugaylotis@gmail.com>
2019-10-30 18:32:02 -0700

    [Make] Deplyment to node.js container preparations

--- src/test/test.js
+++ src/test/test.js
@@ -1,20 +1,20 @@
-import {GameField} from "../index.js"
+import {GameField} from "../gamefield.js"
 var assert = require('assert');
 
 describe('Game logic tests', function() {
     it('Spread game field left and up, change shift', function() {
         let game = new GameField();
-        assert.equal(game.width, 5);
-        assert.equal(game.height, 5);
+        assert.equal(game.getFieldSize().width, 5);
+        assert.equal(game.getFieldSize().height, 5);
         game.turn(0, 0);
-        assert.equal(game.width, 7);
-        assert.equal(game.moves[0].row, 2);
-        assert.equal(game.moves[0].column, 2);
+        assert.equal(game.getFieldSize().width, 8);
+        assert.equal(game.moves[0].row, 3);
+        assert.equal(game.moves[0].column, 3);
     });
     it('Spread game field right and down, without change shift', function() {
         let game = new GameField();
         game.turn(4, 4);
-        assert.equal(game.width, 6);
-        assert.equal(game.height, 6);
+        assert.equal(game.getFieldSize().width, 6);
+        assert.equal(game.getFieldSize().height, 6);
     })
 })
